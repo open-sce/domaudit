0667a407346259422ee5c574e4a59388171ba52c
Vaibhav Dhawan <vaibhav.dhawan@dominodatalab.com>
2023-05-24 18:07:57 +0100

    CLI update
    Added activity to CLI. Also changed inputs for both project and activity CLI to parse owner/project string and convert it to project ID

--- domaudit_cli/domaudit_cli/domaudit_cli.py
+++ domaudit_cli/domaudit_cli/domaudit_cli.py
@@ -6,9 +6,17 @@ import time
 import pandas as pd 
 
 DOM_NAMESPACE = getenv("DOMINO_API_HOST").split(".")[1].split(":")[0]
-DOMAUDIT_HOST = f"http://domaudit.{DOM_NAMESPACE}"
+DOMAUDIT_HOST = getenv("DOMAUDIT_HOST",f"http://domaudit.{DOM_NAMESPACE}")
 USER_AUDIT_PATH = "/user_audit"
 PROJECT_AUDIT_PATH = "/project_audit"
+PROJECT_ACTIVITY_PATH = "/project_activity"
+
+def get_project_id(project_owner, project_name):
+
+    project = make_call(f"{getenv('DOMINO_API_HOST')}/v4/gateway/projects/findProjectByOwnerAndName",
+              {"ownerName": project_owner, "projectName": project_name} )
+    
+    return project['id']
 
 def make_call(host,parameters=None):
 
@@ -43,10 +51,17 @@ def cli():
     user_parser.add_argument("--date-to", help="End date for filter - YYYY-MM-DD format", dest="dateTo")
 
     project_parser = subparsers.add_parser(name="project", help="Project Audit")
-    project_parser.add_argument("--project-id", help="Domino Project ID to audit", default=getenv("DOMINO_PROJECT_ID"))
-    project_parser.add_argument("--project-name", help="Domino Project name to audit", default=getenv("DOMINO_PROJECT_NAME"))
-    project_parser.add_argument("--project-owner", help="Domino Project owner", default=getenv("DOMINO_PROJECT_OWNER"))
+    project_parser.add_argument("--project", help="Domino Project to audit, in the format OWNER/PROJECT", 
+                                default="/".join([getenv("DOMINO_PROJECT_OWNER"),getenv("DOMINO_PROJECT_NAME")]))
     project_parser.add_argument("--links", action=argparse.BooleanOptionalAction, help="Include links back to Domino", default=False)
+
+    activity_parser = subparsers.add_parser(name="activity", help="Project Activity")
+    activity_parser.add_argument("--project", help="Domino Project to audit, in the format OWNER/PROJECT", 
+                                default="/".join([getenv("DOMINO_PROJECT_OWNER"),getenv("DOMINO_PROJECT_NAME")]))
+    activity_parser.add_argument("--page-size", help="Maximum number of rows returned. Default is 500", default="500")
+    activity_parser.add_argument("--latest-event-time", help="End date of the activity report - YYYY-MM-DD format")
+    activity_parser.add_argument("--activity-source", help="Filter by activity source. Valid values: \n\t"
+                                "project, job, model_api, schedule_job, files, workspace, comment, app")
 
 
     if len(sys.argv) <= 1:
@@ -57,11 +72,47 @@ def cli():
     clean_args = args.__dict__.copy()
     clean_args.pop("host")
     clean_args.pop("audit")
-    print(args)
+
     if args.audit == "user":
         output = make_call(f"{DOMAUDIT_HOST}{USER_AUDIT_PATH}",clean_args)
     elif args.audit == "project":
-        output = make_call(f"{DOMAUDIT_HOST}{PROJECT_AUDIT_PATH}",clean_args)
+        split_string = args.project.split("/")
+        if len(split_string)==2:
+            project_owner = split_string[0]
+            project_name = split_string[1]
+            project_id = get_project_id(project_owner,project_name)
+            links = args.links
+        else:
+            print(f"Invalid project name format: {args.project}")
+            project_parser.print_help()
+            exit(1)
+
+        project_args = {
+            "project_id": project_id,
+            "project_name": project_name,
+            "project_owner": project_owner,
+            "links": links
+        }
+        output = make_call(f"{DOMAUDIT_HOST}{PROJECT_AUDIT_PATH}",project_args)
+    elif args.audit == "activity":
+        split_string = args.project.split("/")
+        if len(split_string)==2:
+            project_owner = split_string[0]
+            project_name = split_string[1]
+            project_id = get_project_id(project_owner,project_name)
+        else:
+            print(f"Invalid project name format: {args.project}")
+            activity_parser.print_help()
+            exit(1)
+
+        activity_args = {
+            "project_id": project_id,
+            "page_size": args.page_size,
+            "latest_event_time": args.latest_event_time,
+            "activity_source": args.activity_source
+        }
+        output = make_call(f"{DOMAUDIT_HOST}{PROJECT_ACTIVITY_PATH}",activity_args)
+
 
     write_csv(args.audit, output)
 
